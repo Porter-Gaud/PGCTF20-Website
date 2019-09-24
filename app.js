029fcb333dc576783dfdf584ba0909f9d884940c
Charles Truluck <github@ctruluck.com>
2019-09-24 15:51:01 -0400

    testing grid layout for team

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes/index');
 const app = express();
 app.set('view engine', 'ejs')
 app.use('/', routes)
-app.use('/pic', express.static('assets/images'))
+app.use('/images', express.static('assets/images'))
 app.use('/public', express.static('node_modules/material-design-lite'))
 
 module.exports = app;
