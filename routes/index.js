5b3cf22cd2d62cba922de2838f70ba1a52eedd3a
Charles Truluck <github@ctruluck.com>
2019-09-24 14:52:06 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => { res.render('index') });
+router.get('/statistics', (req, res) => { res.render('statistics') });
+router.get('/about', (req, res) => { res.render('about') });
+router.get('/team', (req, res) => { res.render('team') });
+router.get('/contact', (req, res) => { res.render('contact') });
+router.get('/register', (req, res) => { res.render('register') });
 
 module.exports = router;
