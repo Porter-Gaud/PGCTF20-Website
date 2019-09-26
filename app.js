95ece79509eb5f43cffd7543d001e1247ad27789
Charles Truluck <github@ctruluck.com>
2019-09-26 12:36:34 -0400

    lots of progress

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ app.set('view engine', 'ejs')
 app.use('/', routes)
 app.use('/images', express.static('assets/images'))
 app.use('/public', express.static('node_modules/material-design-lite'))
+app.use('/data', express.static('node_modules/chartist/dist'))
 
 module.exports = app;
